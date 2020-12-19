c777113f7a00d0adc2b87e49055e621063cddc9a
prakhar0010 <72820886+Prakhar00100@users.noreply.github.com>
2020-12-18 23:28:08 -0800

    UC5

--- Emplyeewage.sh
+++ Emplyeewage.sh
@@ -4,7 +4,10 @@ console.log('welcome to Employee Wage Computation')
          const PART_TIME_HOURS = 4;
          const FULL_TIME_HOURS = 8;
          const WAGE_PER_HOUR = 20;
-       
+         const NUM_OF_WORKING_DAYS = 20;
+         const MAX_HRS_IN_MONTH = 100;
+         let totalEmpHrs = 0;
+         let totalWorkingDays = 0;
          function getWorkingHours(empCheck) {
          switch (empCheck) {
                  case IS_FULL_TIME:
@@ -19,8 +22,13 @@ console.log('welcome to Employee Wage Computation')
             }
               let empHrs = 0;
               empCheck = Math.floor(Math.random() * 10) % 3;
-              empHrs = getWorkingHours(empCheck);
-              let empWage = empHrs * WAGE_PER_HOUR;
-              console.log("emp Wage: " + empWage);
+              while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+                totalWorkingDays++;
+                let empCheck = Math.floor(Math.random() * 10) % 3;
+                totalEmpHrs +=  getWorkingHours(empCheck);
+             }
+                let empWage = totalEmpHrs * WAGE_PER_HOUR;
+               console.log("Total Days: " + totalWorkingDays + "Total Hrs: " +totalEmpHrs+ " Emp Wage: " + empWage);
+         
 
 
