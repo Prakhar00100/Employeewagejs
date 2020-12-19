68482578105c41ddb5a9771b7f1f15bb792ff958
prakhar0010 <72820886+Prakhar00100@users.noreply.github.com>
2020-12-18 23:36:40 -0800

    UC6

--- Emplyeewage.sh
+++ Emplyeewage.sh
@@ -5,11 +5,15 @@ console.log('welcome to Employee Wage Computation')
          const FULL_TIME_HOURS = 8;
          const WAGE_PER_HOUR = 20;
          const NUM_OF_WORKING_DAYS = 20;
-         const MAX_HRS_IN_MONTH = 100;
+         const MAX_HRS_IN_MONTH = 160;
+         
+         let empDailyWageArr = new Array();
          let totalEmpHrs = 0;
          let totalWorkingDays = 0;
+
          function getWorkingHours(empCheck) {
-         switch (empCheck) {
+        
+          switch (empCheck) {
                  case IS_FULL_TIME:
                  console.log("Employee is Present");
                  return FULL_TIME_HOURS;
@@ -22,13 +26,21 @@ console.log('welcome to Employee Wage Computation')
             }
               let empHrs = 0;
               empCheck = Math.floor(Math.random() * 10) % 3;
-              while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+
+           function calcDailyWage(empHrs)  {
+                return empHrs *  WAGE_PER_HOUR;
+           }
+           while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
                 totalWorkingDays++;
                 let empCheck = Math.floor(Math.random() * 10) % 3;
-                totalEmpHrs +=  getWorkingHours(empCheck);
-             }
-                let empWage = totalEmpHrs * WAGE_PER_HOUR;
-               console.log("Total Days: " + totalWorkingDays + "Total Hrs: " +totalEmpHrs+ " Emp Wage: " + empWage);
+                let empHrs =  getWorkingHours(empCheck);
+                totalEmpHrs += empHrs;
+                empDailyWageArr.push(calcDailyWage(empHrs));
+           }
+       
+        let empWage = calcDailyWage(totalEmpHrs);
+        console.log(" Total Days: " + totalWorkingDays + "Total Hrs: " +totalEmpHrs+ " Emp Wage: " + empWage);
+ 
          
 
 
