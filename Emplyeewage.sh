6606a6c22dc22341ab17b6eb52ea53c736f740eb
prakhar0010 <72820886+Prakhar00100@users.noreply.github.com>
2020-12-18 21:16:53 -0800

    UC1

--- Emplyeewage.sh
+++ Emplyeewage.sh
@@ -1,1 +1,17 @@
- 
+console.log('welcome to Employee Wage Computation')
+
+ //uc1
+
+const IS_PRESENT = 1;
+      let empCheck = Math.floor(Math.random() * 10) % 2;
+      if (empCheck == IS_PRESENT)
+          {
+           console.log("Employee is Present");
+                return;
+          }
+      else
+         {
+          console.log("Employee is Abscent");
+
+    }
+
