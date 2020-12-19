8a7b73d91680c18b0f61702b75a31d0027a02bb8
prakhar0010 <72820886+Prakhar00100@users.noreply.github.com>
2020-12-18 22:55:31 -0800

    UC3

--- Emplyeewage.sh
+++ Emplyeewage.sh
@@ -1,4 +1,5 @@
 console.log('welcome to Employee Wage Computation')
+
          const IS_PART_TIME = 2;
          const IS_FULL_TIME = 1;
          const PART_TIME_HOURS = 4;
@@ -6,28 +7,22 @@ console.log('welcome to Employee Wage Computation')
          const WAGE_PER_HOUR = 20;
 
        
-         let empHrs = 0;
-
-            empCheck = Math.floor(Math.random() * 10) % 3;
-       
-         switch (empCheck)
-		 {
+         function getWorkingHours(empCheck) {
+         switch (empCheck) {
                  case IS_FULL_TIME:
-                 empHrs = FULL_TIME_HOURS;
                  console.log("Employee is Present");
-                 break;
-
-       
+                 return FULL_TIME_HOURS;
                  case IS_PART_TIME:
-                 empHrs = PART_TIME_HOURS;
                  console.log("Employee is Present");
-                 break;
+                 return PART_TIME_HOURS;
                  default:console.log("Employee is Abscent");
-                  empHrs = 0;
-
-                 }
-
-               let empWage = empHrs * WAGE_PER_HOUR;
-               console.log("emp Wage: " + empWage);
+                  return 0;
+              }
+            }
+              let empHrs = 0;
+              empCheck = Math.floor(Math.random() * 10) % 3;
+              empHrs = getWorkingHours(empCheck);
+              let empWage = empHrs * WAGE_PER_HOUR;
+              console.log("emp Wage: " + empWage);
 
 
